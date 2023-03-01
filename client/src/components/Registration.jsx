a8e4dcebec9b1445d9de4d8ea0910bcd69bd2de3
Kate Anderson <kaanders00@gmail.com>
2023-03-01 10:39:41 -0500

    full login and reg changes from reviouous day

--- client/src/components/Registration.jsx
+++ client/src/components/Registration.jsx
@@ -1,48 +1,56 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 
 const Registration = () => {
 
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
-    const [username, setUsername] = useState("");
+    const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
-    const [confPassword, setConfPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [errors,setErrors]= useState([])
     const navigate= useNavigate()
 
+
     const postRegistrationDetails = () => {
-        fetch("http://localhost:8000/api/register", {
-            method: "POST",
-            body: JSON.stringify({
-                firstName, lastName, username, password, confPassword
-            }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            if (data.error_message) {
-                alert(data.error_message);
-            } else {
-                alert(data.message);
-                navigate("/");
-            }
-        })
-        .catch((err) => console.error(err));
-    };
+        // if (password == confirmPassword){
+            axios.post("http://localhost:8000/api/register", {
+                firstName,lastName,userName, password, confirmPassword
+            })
+
+                .then(res =>{
+                    navigate('/search')
+                })
+                .then((res) => res.json())
+                .then((data) => {
+                    if (data.error_message) {
+                        alert(data.error_message);
+                    } else {
+                        alert(data.message);
+                        navigate("/");
+                    }
+                })
+            .catch((err) => console.error(err));
+    // } else{
+    //     alert("bruh pws need to match")
+    // }
+}
+    
+        
 
     const handleSubmit = (e) => {
         e.preventDefault();
         postRegistrationDetails();
-
-        console.log({ firstName, lastName, username, password, confPassword });
+        console.log({ firstName, lastName, userName, password, 
+            confirmPassword
+        });
         setFirstName("");
         setLastName("");
-        setUsername("");
+        setUserName("");
         setPassword("");
-        setConfPassword("");
+        setConfirmPassword("");
 
     };
 
@@ -50,8 +58,6 @@ const Registration = () => {
     return (
         <div className=' registration_containter'>
         <form className="register p-4 mt-5" onSubmit={handleSubmit}>
-
-        {/* <form className='register mt-5 w-50 mx-auto p-4 ' onSubmit={handleSubmit}> */}
             <h2 className='text-white mb-3'>Register</h2>
             <input 
                 placeholder='First Name' 
@@ -61,6 +67,11 @@ const Registration = () => {
                 required
                 onChange={(e) => setFirstName(e.target.value)}
             />
+                {
+                    errors?.firstName && (
+                        <span className="text-danger">{errors.firstName?.message}</span>
+                    )
+                }
             <input 
                 placeholder='Last Name' 
                 className='form-control  my-2' 
@@ -69,14 +80,24 @@ const Registration = () => {
                 required
                 onChange={(e) => setLastName(e.target.value)}
             />
+                {
+                    errors?.lastName && (
+                        <span className="text-danger">{errors.lastName?.message}</span>
+                    )
+                }
             <input 
                 placeholder='Username' 
                 className='form-control  my-2' 
                 type="text" 
-                value={username}
+                value={userName}
                 required
-                onChange={(e) => setUsername(e.target.value)}
+                onChange={(e) => setUserName(e.target.value)}
             />
+                {/* {
+                    errors?.userName && (
+                        <span className="text-danger">{errors.userName?.message}</span>
+                    )
+                } */}
             <input 
                 placeholder='Password' 
                 className='form-control  my-2' 
@@ -86,16 +107,24 @@ const Registration = () => {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
             />
+                {/* {
+                    errors?.password && (
+                        <span className="text-danger">{errors.password?.message}</span>
+                    )
+                } */}
             <input 
                 placeholder='Confirm Password' 
                 className='form-control  my-2' 
                 type="password" 
+                minLength={8}
+                required
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
             />
             <button className='btn btn-success mt-2'>Register</button>
         </form>
         </div>
     )
 }
-//testing
 
 export default Registration
--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const cors = require("cors")
 const port = 8000;
+require('dotenv').config()
+const jwt = require("jsonwebtoken")
+const cookieParser = require('cookie-parser')
 
 const app = express();
 app.use(express.json(), express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors({
+    credentials:true,
+    origiin: 'http://localhost:3000'
+}))
+
+app.use(express.json())
+app.use(cookieParser())
 
 require("./config/mongoose.config");
 require("./routes/user.routes")(app); // may need to adjust routes here
 
 // must be at the bottom
-app.listen(port, () => console.log(`Listening on port: ${port}`))  
+app.listen(port, () => console.log(`Listening on port: ${port}`))
